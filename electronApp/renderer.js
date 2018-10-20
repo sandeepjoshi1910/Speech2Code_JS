096ce38f43ba23dd759772991362d2fa8dea202a
Sandeep Joshi <aw.snjoshi@hotmail.com>
2018-10-19 21:48:02 -0400

    Fixed errors

--- electronApp/renderer.js
+++ electronApp/renderer.js
@@ -1,7 +1,7 @@
 
 var amqp = require('amqplib/callback_api');
 
-const actButton = document.getElementById('startActivity');
+const actButton = document.getElementById('SendAction');
 const messagetxt = document.getElementById('message')
 
 //Queues
@@ -12,8 +12,9 @@ actButton.addEventListener('click',(event)=> {
     amqp.connect('amqp://localhost', function(err, conn) {
         conn.createChannel(function(err, ch) {
             var msg = 'Hello World! from electron app';
+            q = sendQ
 
-            ch.assertQueue(sendQ, {durable: false});
+            ch.assertQueue(q, {durable: false});
             ch.sendToQueue(q, Buffer.from(msg));
             console.log(" [x] Sent %s", msg);
         });
@@ -26,8 +27,8 @@ actButton.addEventListener('click',(event)=> {
 // Receiving the message
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-
-    ch.assertQueue(recvQ, {durable: false});
+    q = recvQ
+    ch.assertQueue(q, {durable: false});
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
     ch.consume(q, function(msg) {
       console.log(" [x] Received %s", msg.content.toString());

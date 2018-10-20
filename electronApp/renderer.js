0c07514e574e3283cdd4804043adca31e8ec528a
Sandeep Joshi <aw.snjoshi@hotmail.com>
2018-10-19 21:22:02 -0400

    Added UI elements and renderer to send and receive messages

--- electronApp/renderer.js
+++ electronApp/renderer.js
@@ -1,3 +1,37 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+
+var amqp = require('amqplib/callback_api');
+
+const actButton = document.getElementById('startActivity');
+const messagetxt = document.getElementById('message')
+
+//Queues
+const sendQ = "ele_to_py"
+const recvQ = "py_to_ele"
+
+actButton.addEventListener('click',(event)=> {
+    amqp.connect('amqp://localhost', function(err, conn) {
+        conn.createChannel(function(err, ch) {
+            var msg = 'Hello World! from electron app';
+
+            ch.assertQueue(sendQ, {durable: false});
+            ch.sendToQueue(q, Buffer.from(msg));
+            console.log(" [x] Sent %s", msg);
+        });
+    });
+});
+
+
+
+
+// Receiving the message
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+
+    ch.assertQueue(recvQ, {durable: false});
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+    ch.consume(q, function(msg) {
+      console.log(" [x] Received %s", msg.content.toString());
+      messagetxt.innerHTML = msg.content.toString();
+    }, {noAck: true});
+  });
+});

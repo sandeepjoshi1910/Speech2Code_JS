40406538b7128929b2439ee1837446308e7cbacc
Shubadra Govindan <sgovin9@uic.edu>
2018-10-21 01:55:12 -0500

    Added audio clips for actions

--- electronApp/renderer.js
+++ electronApp/renderer.js
@@ -66,6 +66,48 @@ amqp.connect('amqp://localhost', function(err, conn) {
         else if (action_name == 'Function Creation Returned') {
           play_audio('add_function.mp3');
         }
+        else if (action_name.ignoreCase == 'if-else Block Created') {
+          play_audio('add_if_else.mp3');
+        }
+        else if (action_name.ignoreCase == 'Main Function Created') {
+          play_audio('add_main.mp3');
+        }
+        else if (action_name.ignoreCase == 'While statement added.') {
+          play_audio('add_while.mp3');
+        }
+        else if (action_name.ignoreCase == 'Go to line/function called.') {
+          play_audio('goto.mp3');
+        }
+        else if (action_name.ignoreCase == 'Function called.') {
+          play_audio('call_function.mp3');
+        }
+        else if (action_name.ignoreCase == 'Variable added.') {
+          play_audio('add_variables.mp3');
+        }
+        else if (action_name.ignoreCase == 'Breakpoint added.') {
+          play_audio('create_breakpoint.mp3');
+        }
+        else if (action_name.ignoreCase == 'class added/created.') {
+          play_audio('add_class.mp3');
+        }
+        else if (action_name.ignoreCase == 'NewLine inserted.') {
+          play_audio('add_newline.mp3');
+        }
+        else if (action_name.ignoreCase == 'Exception Handling block added.') {
+          play_audio('add_try_catch.mp3');
+        }
+        else if (action_name.ignoreCase == 'Line Removed') {
+          play_audio('delete_line.mp3');
+        }
+        else if (action_name.ignoreCase == 'Running script') {
+          play_audio('run_file.mp3');
+        }
+        else if (action_name.ignoreCase == 'Changes reverted back (Undo).') {
+          play_audio('undo_changes.mp3');
+        }
+        else if (action_name.ignoreCase == 'Invalid query') {
+          play_audio('invalid.mp3');
+        }
       }
       else{
         messagetxt.innerHTML = received_msg.toString();
